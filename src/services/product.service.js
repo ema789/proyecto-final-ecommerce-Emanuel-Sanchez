d23a101a02232c6b55d70c17a498361df324334e
ema789 <70720306+ema789@users.noreply.github.com>
2025-07-15 16:23:44 -0300

    Update product.service.js

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,16 +1,1 @@
-import fs from 'fs/promises';
-const filePath = './data/products.json';
 
-async function readData(){
-
-    const data = await fs.readFile(filePath, 'utf-8');
-    return JSON.parse(data);
-
-}
-
-async function writeData(data){
-    await fs.writeFile(filePath, JSON.stringify(data, null, 2));
-}
-export const getAllproducts = ()=>{
-    return readData();
-};
